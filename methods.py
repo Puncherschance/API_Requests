4a8787b14d423424115fa7eb37f80d7dfb21ef7f
Vladislav Krasheninnikov <vladislav.krash187@gmail.com>
2023-11-23 17:29:04 +0400

    Начало работы с Allure

--- methods.py
+++ methods.py
@@ -1,5 +1,6 @@
 from jsonschema import validate
 import requests
+import allure
 import json
 from faker import Faker
 from log import Logger
@@ -11,26 +12,31 @@ class Api:
 
     @staticmethod
     def send_get_request(url):
-        return requests.get(url)
+        with allure.step("Send GET request to Server."):
+            return requests.get(url)
 
     @staticmethod
     def send_post_request(url, data):
-        headers = {"Content-type": "application/json; charset=UTF-8"}
-        return requests.post(url, data=data, headers=headers)
+        with allure.step("Send POST request to Server."):
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            return requests.post(url, data=data, headers=headers)
 
     @staticmethod
     def send_put_request(url, data):
-        headers = {"Content-type": "application/json; charset=UTF-8"}
-        return requests.put(url, data=data, headers=headers)
+        with allure.step("Send PUT request to Server."):
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            return requests.put(url, data=data, headers=headers)
 
     @staticmethod
     def send_patch_request(url, data):
-        headers = {"Content-type": "application/json; charset=UTF-8"}
-        return requests.patch(url, data=data, headers=headers)
+        with allure.step("Send PATCH request to Server."):
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            return requests.patch(url, data=data, headers=headers)
 
     @staticmethod
     def send_delete_request(url):
-        return requests.delete(url)
+        with allure.step("Send DELETE request to Server."):
+            return requests.delete(url)
 
     @staticmethod
     def text_to_json(text):
@@ -48,30 +54,32 @@ class Api:
 
     @staticmethod
     def validate_schema(res):
-        def compare(res):
-            try:
-                api = Api()  # как убрать?
-                validate(api.text_to_json(res.text), api.load_schema("get_specific_post.json"))
-                return True
-            except Exception:
-                logger = Logger()  # как убрать?
-                logger.log_exception(error_message)
-                return False
-        error_message = "Json is incorrect!"
-        assert compare(res), error_message
+        with allure.step("Validate received JSON Schema."):
+            def compare(res):
+                try:
+                    api = Api()  # как убрать?
+                    validate(api.text_to_json(res.text), api.load_schema("get_specific_post.json"))
+                    return True
+                except Exception:
+                    logger = Logger()  # как убрать?
+                    logger.log_exception(error_message)
+                    return False
+            error_message = "Json is incorrect!"
+            assert compare(res), error_message
 
     @staticmethod
     def validate_status_code(actual, expected):
-        def compare(actual, expected):
-            try:
-                assert actual.status_code == expected
-                return True
-            except Exception:
-                logger = Logger()  # как убрать?
-                logger.log_exception(error_message)
-                return False
-        error_message = f"Status Code {actual.status_code} is not equal with {expected}!"
-        assert compare(actual, expected), error_message
+        with allure.step("Validate received Status Code."):
+            def compare(actual, expected):
+                try:
+                    assert actual.status_code == expected
+                    return True
+                except Exception:
+                    logger = Logger()  # как убрать?
+                    logger.log_exception(error_message)
+                    return False
+            error_message = f"Status Code {actual.status_code} is not equal with {expected}!"
+            assert compare(actual, expected), error_message
 
 
 class Fake:
--- test_steps.py
+++ test_steps.py
@@ -1,15 +1,19 @@
 import pytest
+import allure
 from env import *
 
 
 @pytest.mark.smoke
+@allure.feature("GET Requests")
 class TestGetRequests:
 
+    @allure.story("GET all available Posts")
     def test_get_all_posts(self, api, write_log):
         res = api.send_get_request(Env.url)
         api.validate_status_code(res, 200)
         write_log(res)
 
+    @allure.story("GET the specific Post")
     @pytest.mark.parametrize("post_id", ("1", "2", "3", "4", "5"))
     def test_get_specific_post(self, api, post_id, write_log):
         res = api.send_get_request(Env.url+post_id)
@@ -19,8 +23,10 @@ class TestGetRequests:
 
 
 @pytest.mark.smoke
+@allure.feature("POST Requests")
 class TestPostRequests:
 
+    @allure.story("POST new Post")
     @pytest.mark.parametrize("user_id", (1, 2, 3, 4, 5))
     def test_post_new_post(self, api, fake, user_id, write_log):
         fake_data = fake.generate_fake_data(user_id=user_id)
@@ -31,8 +37,10 @@ class TestPostRequests:
 
 
 @pytest.mark.smoke
+@allure.feature("PUT Requests")
 class TestPutRequests:
 
+    @allure.story("PUT in the specific Post")
     @pytest.mark.parametrize("post_id", ("1", "2", "3", "4", "5"))
     def test_put_in_post(self, api, fake, post_id, write_log):
         fake_data = fake.generate_fake_data(post_id=int(post_id))
@@ -43,8 +51,10 @@ class TestPutRequests:
 
 
 @pytest.mark.smoke
+@allure.feature("PATCH Requests")
 class TestPatchRequests:
 
+    @allure.story("PATCH the specific Post")
     @pytest.mark.parametrize("post_id", ("1", "2", "3", "4", "5"))
     def test_patch_post(self, api, post_id, write_log):
         patched_title = api.patch_title()
@@ -55,8 +65,10 @@ class TestPatchRequests:
 
 
 @pytest.mark.smoke
+@allure.feature("DELETE Requests")
 class TestDeleteRequests:
 
+    @allure.story("DELETE the specific Post")
     @pytest.mark.parametrize("post_id", ("1", "2", "3", "4", "5"))
     def test_delete_post(self, api, post_id, write_log):
         res = api.send_delete_request(Env.url+post_id)
